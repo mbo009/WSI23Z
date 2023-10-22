1cf5c62b8f1bd8e2220a66fa4eae81cd2e3db640
mciesla <miloszc009@gmail.com>
2023-10-22 21:02:56 +0200

    New measurements in report

--- LAB01/measurements.py
+++ LAB01/measurements.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from mySolver import mySolver
-import functions
 
 
 def stepLenDependency(f, fGradient, precision, stepLen, maxDepth, x0):
@@ -11,7 +10,6 @@ def stepLenDependency(f, fGradient, precision, stepLen, maxDepth, x0):
     results = []
     for index in range(1000):
         solverResult = mySolver(stepLen * (index + 1), precision, maxDepth).solve(f, fGradient, [x0])
-        # print(index, solverResult)
         results.append(solverResult)
 
     return results
@@ -64,15 +62,6 @@ def randXMeasurements(f, fGradient, precision, stepLen, maxDepth, ammountRandX):
         listX.append(randX)
         result = stepLenDependency(f, fGradient, precision, stepLen, maxDepth, randX)
         for index in range(1000):
-            results[index][0][0] += result[index][0][0]
-            results[index][1] += result[index][1]
-    print(listX)
-    return results
-
-
-# measurements = randXMeasurements(functions.f, functions.fGradient, 1e-4, 0.001, 1e8, 10)
-# measurements = stepLenDependency(functions.f, functions.fGradient, 1e-4, 0.001, 1e8, 1)
-# plt_stepLen(measurements, "Liczba wymaganych kroków do długości", 0.001)
-# plt_function(functions.fGradient, True)
-# plt_stepLen(stepLenDependency(functions.f, functions.fGradient, 1e-5, 0.001, 1e6, 1), "Wykres", 0.001)
-# plt_accuracy(stepLenDependency(functions.f, functions.fGradient, 1e-5, 0.001, 1e6, 1), 0, "Wykres", 0.001)
+            results[index][0][0] += result[index][0][0]/10
+            results[index][1] += result[index][1]/ammountRandX
+    return [results, listX]
